54754b64dcf3f3d233c86863bfe97a1267c3bc16
Jozue06 <jozue06@gmail.com>
2018-06-29 18:28:00 -0700

    cleaning of numbers

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const fs = require('fs');
 let root = __dirname;
 
 function getArgs(originalFile, newFile, transformationString) {
-//   if (originalFile.split('.')[originalFile.split('.').length - 1] !== 'bmp' || newFile.split('.')[newFile.split('.').length - 1] !== 'bmp') {
-//     console.log(originalFile.split('.')[originalFile.split('.').length - 1]);
-//     console.log('please use a file that ends in .bmp');
-//     return 'error';
-//   } else {
+  if (originalFile.split('.')[originalFile.split('.').length - 1] !== 'txt' || newFile.split('.')[newFile.split('.').length - 1] !== 'txt') {
+    console.log(originalFile.split('.')[originalFile.split('.').length - 1]);
+    console.log('please use a file that ends in .txt');
+    return 'error';
+  } else {
 
-  fs.access(`${root}/${originalFile}`, fs.constants.R_OK, (err) => {
-    if (err) {
-      console.log('please use an existing file ');
-      return 'error';
-    } else {
-      app(originalFile, newFile, transformationString);
-    }
-  });
+    fs.access(`${root}/${originalFile}`, fs.constants.R_OK, (err) => {
+      if (err) {
+        console.log('please use an existing file ');
+        return 'error';
+      } else {
+        app(originalFile, newFile, transformationString);
+      }
+    });
+  }
 }
-// }
 
 getArgs(process.argv[2], process.argv[3], process.argv[4]);
 
--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const root = __dirname;
 console.log('hi');
 
 function app(oldPath, newPath, command) {
-  let transformation;
+  // let transformation;
 
   switch (command) {
   case 'test':
@@ -31,22 +31,44 @@ function app(oldPath, newPath, command) {
     if (err) {
       console.log('!!ERROR!! 2222 ', err);
     } else {
-      console.log('buff man', data);
-      // let cleaned = new CleanBuffer(data);
+      let scores = [];
+      let re = '\n';
+      let cleanedScore = data.toString().trim().split(re);
+      
+      cleanedScore.forEach( (e) => {
+        // let num = ;
+        scores.push(e.match(/\d/g));
+        return scores;
+      });
 
+      scores.forEach((calc) =>{
+        if( calc[0] > calc[1]){ 
+          return console.log('team 1 won');}
+        if( calc[0] <calc[1]){ 
+          return console.log('team 2 won');}
+        if( calc[0] === calc[1]){ 
+          return console.log('teams tied');}
+
+      });
+      // console.log('scores array -->' , scores);
+      // .match(/\d/g);
+      // let num = cleanedScore.match(/\d/g);
+      // num = num.join(' , ');
+  
       // var newBuffer = transformation();
 
-      write(`${root}/${newPath}`, data, (err) => {
+      write(`${root}/${newPath}`, scores, (err) => {
         if (err) console.log('!!ERROR!!3333');
         else {
           console.log(command +  ' finished to ' + newPath);
         }
       });
-
     }
   });
-
 }
+
+
+
 
 module.exports = app;
 
