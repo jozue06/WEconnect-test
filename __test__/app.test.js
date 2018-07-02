ebfe1acf7eea3d939b374d5c81792091f8034240
Jozue06 <jozue06@gmail.com>
2018-07-02 00:31:24 -0700

    testings

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,15 +1,49 @@
 'use strict';
 
+const fs = require('fs');
 const app = require('../src/app.js');
-// const root = __dirname + '../../';
+
 
 describe ('app module testing,', () => {
 
-  it('should test the app functionality of input whith a correct command', () => {
+  it('should test the app functionality of the CLI input with correct syntax. A good result sound be undifined and will write new fill.', (done) => {
 
-    expect(app('src/assets/sample-input.txt src/assets/sample-out.txt run')).toBe('error');
+    setTimeout( () => {
+      let results = app('../src/assets/sample-input.txt', '../src/assets/sample-outs.txt', 'run');
+      expect(results).toBeUndefined();   
+      done();
+    }, 400);
+  });
+  
+
+  it('should read the new file created by the above test of the app.', (done) =>{
+
+    setTimeout( () => {
+      fs.readFile('../src/assets/sample-outs.txt', (err, fileCheck) => {
+        let fileCheck2 = fileCheck;
+        if (err) {
+          console.log('error  ', err);
+          return err;
+        }      
+        expect(fileCheck).toBe(fileCheck2);
+        done();
+      });
+    }, 400);
+
   });
 
-  
+
+  it('should fail when trying to read an incorrect filepath or non-existant file', (done) =>{
+
+    setTimeout( () => {
+      fs.readFile('../src/assets/sample-outsss.txt', (err) => {
+        if (err) {
+          expect(err).toBeDefined();
+          done();
+        }    
+      });
+    }, 400);
+
+  });
 
 });
