043a9bbf28732d4fae96af93fc63d0b2bf3a495b
Jozue06 <jozue06@gmail.com>
2018-07-02 00:56:11 -0700

    travis

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -9,7 +9,7 @@ describe ('app module testing,', () => {
   it('should test the app functionality of the CLI input with correct syntax. A good result sound be undifined and will write new fill.', (done) => {
 
     setTimeout( () => {
-      let results = app('../src/assets/sample-input.txt', '../src/assets/sample-outs.txt', 'run');
+      let results = app('./src/assets/sample-input.txt', './src/assets/sample-outs.txt', 'run');
       expect(results).toBeUndefined();   
       done();
     }, 400);
@@ -19,7 +19,7 @@ describe ('app module testing,', () => {
   it('should read the new file created by the above test of the app.', (done) =>{
 
     setTimeout( () => {
-      fs.readFile('../src/assets/sample-outs.txt', (err, fileCheck) => {
+      fs.readFile('./src/assets/sample-outs.txt', (err, fileCheck) => {
         let fileCheck2 = fileCheck;
         if (err) {
           console.log('error  ', err);
@@ -36,7 +36,7 @@ describe ('app module testing,', () => {
   it('should fail when trying to read an incorrect filepath or non-existant file', (done) =>{
 
     setTimeout( () => {
-      fs.readFile('../src/assets/sample-outsss.txt', (err) => {
+      fs.readFile('./src/assets/sample-outsss.txt', (err) => {
         if (err) {
           expect(err).toBeDefined();
           done();
